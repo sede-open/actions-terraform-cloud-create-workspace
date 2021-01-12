f19bca5359ef439b3ad7cd195eceeefa620aa898
Nathan Shire <nathan.shire@gmail.com>
2021-01-11 21:00:40 -0600

    check for valid exec_mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { exec } = require('child_process');
 const https = require('https')
 
 try {
@@ -8,12 +9,17 @@ try {
   const name = core.getInput('name')
   const org = core.getInput('org')
   const desc = core.getInput('desc')
-  const exec_mode = core.getInput('exec_mode')
-  const tf_version = core.getInput('tf_version')
+  var exec_mode = core.getInput('exec_mode')
+  var tf_version = core.getInput('tf_version')
   const env = core.getInput('env')
 
-  if (exec_mode == null) {
+  if (!exec_mode) {
     exec_mode = 'remote'
+  } else {
+    exec_mode = exec_mode.toLocaleLowerCase()
+  }
+  if (exec_mode != 'local' && exec_mode != 'remote') {
+    throw ('Invalid value for "exec-mode"')
   }
 
   // Build JSON Payload with Inputs
@@ -24,7 +30,7 @@ try {
         "description": desc,
         "environment": env,
         "readme": null,
-        "execution-mode": exec_mode,
+        "execution-mode": exec_mode.toLowerCase(),
         "auto-apply": false,
         "working-directory": "",
         "terraform-version": tf_version,
@@ -66,17 +72,20 @@ try {
       console.log(`Unauthorized to created workspace in the "${org}" organization.`)
     } else if (res.statusCode == '404') {
       console.log(`Organization "${org}" not found in Terraform Cloud.`)
-    } else if (res.statusCode == '422') {
-      console.log(`Workspace named "${name}" already exists in the "${org}" organization.`)  
     } else {
       res.on('data', d => {
-        process.stdout.write(d)
+        if (d.includes('Name has already been taken')) {
+          console.log(`Workspace named "${name}" already exists in the "${org}" organization.`) 
+        } else {
+          process.stdout.write(d)
+        }
       })
     }
+    return res
   })
   req.write(data)
   req.end()
-
 } catch (error) {
+  console.log(error)
   core.setFailed(error.message)
 }
